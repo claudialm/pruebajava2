b29c7c83538b25827b4cd6a261d73abfc4f04d95
claudialm <claudia_incondicional@hotmail.com>
2017-11-06 19:50:51 -0300

    Creando clase estructura while.

---
+++ src/ClaseContraseña.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *Nombre: Claudia Veas M.
+ * @author LAB03
+ * Fecha: Noviembre 06 de 2017
+ * Programa: ClaseContraseña
+ * Responsabilidad: Clase Principal
+ */
+public class ClaseContraseña 
+{
+   public static void main(String[] args)
+   {
+   int contraseña= numero;
+   int index = 2403;
+   while(index==3)
+    {
+        //stop = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero"));
+           System.out.println(index);
+           index++;
+           
+   }
+    
+    contraseña = Integer.parseInt(JOptionPane.showInputDialog("Ingrese numero"));
+
+    if (numero!=contraseña)
+    {
+    JOptionPane.showMessageDialog(null, "Intente nuevamente");
+    } 
+    else
+    {
+    
+    
+         if (contraseña==) 
+       {
+         JOptionPane.showMessageDialog(null, "Intente nuevamente");  
+       }
+   
+         else
+         {
+         
+         }
+       if (contraseña!=) 
+       {
+           
+       }
+   
+   
+    }
+   
+   }  
+
+}
